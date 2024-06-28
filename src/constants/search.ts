f02ab77be4be68f68efe1d7d9467d89438523c59
Ivo <ivo_gavran@hotmail.com>
2024-06-28 15:30:29 +0200

    refactor: reorganized constants

---
+++ src/constants/search.ts
@@ -0,0 +1,6 @@
+export const OPTIONS = [
+  { value: 'Last viewed', label: 'Last viewed' },
+  { value: 'Most installed', label: 'Most installed' },
+  { value: 'Newest', label: 'Newest' },
+  { value: 'Oldest', label: 'Oldest' },
+];
---
+++ src/constants/templates.ts
@@ -0,0 +1,6 @@
+export const TEMPLATE_CHIPS = [
+  { label: 'Template', color: '#EB2F96', background: '#FFADD2' },
+  { label: 'AWS', color: '#FA8C16', background: '#FFD591' },
+  { label: 'Azure', color: '#2F54EB', background: '#ADC6FF' },
+  { label: 'IBM', color: '#52C41A', background: '#B7EB8F' },
+];
